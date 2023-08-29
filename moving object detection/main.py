a1d35a9183c088d3b33ac26a805382472abeabb9
Zeki Burak ÖZDEN <133498595+burakOzden1@users.noreply.github.com>
2023-08-29 19:03:57 +0300

    Update main.py

--- moving object detection/main.py
+++ moving object detection/main.py
@@ -1,1 +1,21 @@
+import cv2
 
+cap = cv2.VideoCapture("carBike5.mp4")
+
+movingObjects = cv2.createBackgroundSubtractorMOG2()
+# Hareketli nesneleri algılamaya yardımcı ilk maskemizi uyguladık
+
+while True:
+    ret, frame = cap.read()
+
+    mask = movingObjects.apply(frame)
+
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Moving", mask)
+
+    keyCode = cv2.waitKey(10)
+
+    if cv2.getWindowProperty("Frame", cv2.WND_PROP_VISIBLE) < 1:
+        break
+
+cv2.destroyAllWindows()
